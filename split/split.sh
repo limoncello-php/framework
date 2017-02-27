e0d16303eeee7909a77c195cf9abf80cb50d9406
EK <ek@neomerx.com>
2017-02-27 20:02:54 +0300

    #36 Initial commit

--- split/split.sh
+++ split/split.sh
@@ -3,4 +3,5 @@
 git subsplit init git@github.com:limoncello-php/framework.git && \
 git subsplit publish --heads="master" --no-tags components/OAuthServer/:git@github.com:limoncello-php-dist/oauth-server.git &&\
 git subsplit publish --heads="master" --no-tags components/Contracts/:git@github.com:limoncello-php-dist/contracts.git &&\
+git subsplit publish --heads="master" --no-tags components/Passport/:git@github.com:limoncello-php-dist/passport.git &&\
 rm -rf .subsplit/
