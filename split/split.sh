26e2ec590db7873610b954259179938605c34228
EK <ek@neomerx.com>
2017-03-15 12:52:22 +0300

    Add split for Container component

--- split/split.sh
+++ split/split.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 git subsplit init git@github.com:limoncello-php/framework.git && \
-git subsplit publish --heads="master" --no-tags components/OAuthServer/:git@github.com:limoncello-php-dist/oauth-server.git &&\
+git subsplit publish --heads="master" --no-tags components/Container/:git@github.com:limoncello-php-dist/container.git &&\
 git subsplit publish --heads="master" --no-tags components/Contracts/:git@github.com:limoncello-php-dist/contracts.git &&\
+git subsplit publish --heads="master" --no-tags components/OAuthServer/:git@github.com:limoncello-php-dist/oauth-server.git &&\
 git subsplit publish --heads="master" --no-tags components/Passport/:git@github.com:limoncello-php-dist/passport.git &&\
 rm -rf .subsplit/
