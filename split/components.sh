3eebe1fd316884416e014400226db0f96cbd6313
EK <ek@neomerx.com>
2017-03-16 20:32:24 +0300

    Add App Cache component.

--- split/components.sh
+++ split/components.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 COMPONENTS=(
+    'components/AppCache/:git@github.com:limoncello-php-dist/app-cache.git'
     'components/Container/:git@github.com:limoncello-php-dist/container.git'
     'components/Contracts/:git@github.com:limoncello-php-dist/contracts.git'
     'components/Core/:git@github.com:limoncello-php-dist/core.git'
