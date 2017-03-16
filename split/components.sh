5c80336937deeb6a58f39ae8ec5bb325d7d6b007
EK <ek@neomerx.com>
2017-03-17 02:44:26 +0300

    Add Flute (JSON API) component.

--- split/components.sh
+++ split/components.sh
@@ -7,6 +7,7 @@ COMPONENTS=(
     'components/Contracts/:git@github.com:limoncello-php-dist/contracts.git'
     'components/Core/:git@github.com:limoncello-php-dist/core.git'
     'components/Crypt/:git@github.com:limoncello-php-dist/crypt.git'
+    'components/Flute/:git@github.com:limoncello-php-dist/flute.git'
     'components/OAuthServer/:git@github.com:limoncello-php-dist/oauth-server.git'
     'components/Passport/:git@github.com:limoncello-php-dist/passport.git'
     'components/Validation/:git@github.com:limoncello-php-dist/validation.git'
