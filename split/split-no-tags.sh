af249c3beeaca77635aa0fc15e13c0d02e3e54e2
EK <ek@neomerx.com>
2017-03-16 17:40:02 +0300

    Added Core component.

--- split/split-no-tags.sh
+++ split/split-no-tags.sh
@@ -3,6 +3,7 @@
 git subsplit init git@github.com:limoncello-php/framework.git && \
 git subsplit publish --heads="master" --no-tags components/Container/:git@github.com:limoncello-php-dist/container.git &&\
 git subsplit publish --heads="master" --no-tags components/Contracts/:git@github.com:limoncello-php-dist/contracts.git &&\
+git subsplit publish --heads="master" --no-tags components/Core/:git@github.com:limoncello-php-dist/core.git &&\
 git subsplit publish --heads="master" --no-tags components/OAuthServer/:git@github.com:limoncello-php-dist/oauth-server.git &&\
 git subsplit publish --heads="master" --no-tags components/Passport/:git@github.com:limoncello-php-dist/passport.git &&\
 rm -rf .subsplit/
