84e803281dda93e3242c6e34b169a18e1b831ed1
EK <ek@neomerx.com>
2017-03-16 21:36:16 +0300

    Add Crypt component.

--- split/components.sh
+++ split/components.sh
@@ -6,6 +6,7 @@ COMPONENTS=(
     'components/Container/:git@github.com:limoncello-php-dist/container.git'
     'components/Contracts/:git@github.com:limoncello-php-dist/contracts.git'
     'components/Core/:git@github.com:limoncello-php-dist/core.git'
+    'components/Crypt/:git@github.com:limoncello-php-dist/crypt.git'
     'components/OAuthServer/:git@github.com:limoncello-php-dist/oauth-server.git'
     'components/Passport/:git@github.com:limoncello-php-dist/passport.git'
 )
