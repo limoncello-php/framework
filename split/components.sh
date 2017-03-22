44fe45eb4c6646040d435320421c6155533b6d38
EK <ek@neomerx.com>
2017-03-22 19:17:25 +0300

    Added Events component.

--- split/components.sh
+++ split/components.sh
@@ -7,6 +7,7 @@ COMPONENTS=(
     'components/Contracts/:git@github.com:limoncello-php-dist/contracts.git'
     'components/Core/:git@github.com:limoncello-php-dist/core.git'
     'components/Crypt/:git@github.com:limoncello-php-dist/crypt.git'
+    'components/Events/:git@github.com:limoncello-php-dist/events.git'
     'components/Flute/:git@github.com:limoncello-php-dist/flute.git'
     'components/L10n/:git@github.com:limoncello-php-dist/l10n.git'
     'components/OAuthServer/:git@github.com:limoncello-php-dist/oauth-server.git'
