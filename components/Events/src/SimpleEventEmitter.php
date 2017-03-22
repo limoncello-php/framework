932675e157ec76385dd0c1fc2d9d7dab641cb7c8
EK <ek@neomerx.com>
2017-03-22 19:09:03 +0300

    Initial commit.

---
+++ components/Events/src/SimpleEventEmitter.php
@@ -0,0 +1,307 @@
+<?php namespace Limoncello\Events;
+
+/**
+ * Copyright 2015-2017 info@neomerx.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+use Closure;
+use Limoncello\Events\Contracts\EventEmitterInterface;
+use Limoncello\Events\Exceptions\EventNotFoundException;
+use ReflectionException;
+use ReflectionMethod;
+
+/**
+ * @package Limoncello\Events
+ */
+class SimpleEventEmitter implements EventEmitterInterface
+{
+    /**
+     * @var array
+     */
+    private $subscribers = [];
+
+    /**
+     * @var bool
+     */
+    private $cancellingEnabled = false;
+
+    /**
+     * @inheritdoc
+     */
+    public function emit(string $eventName, array $arguments = [])
+    {
+        $this->isCancellingEnabled() === true ?
+            $this->emitWithCancellingPropagationCheck($eventName, $arguments) :
+            $this->emitWithoutCancellingPropagationCheck($eventName, $arguments);
+    }
+
+    /**
+     * @param string   $eventName
+     * @param callable $subscriber
+     *
+     * @return SimpleEventEmitter
+     *
+     * @SuppressWarnings(PHPMD.ElseExpression)
+     */
+    public function subscribe(string $eventName, callable $subscriber): SimpleEventEmitter
+    {
+        if ($subscriber instanceof Closure || ($staticMethod = $this->parseStaticMethod($subscriber)) === null) {
+            $this->subscribers[$eventName][] = $subscriber;
+        } else {
+            assert($staticMethod !== null);
+            $this->subscribers[$eventName][] = $this->getUnifiedStaticMethodRepresentation($staticMethod);
+        }
+
+        return $this;
+    }
+
+    /**
+     * @param string   $eventName
+     * @param callable $subscriber
+     *
+     * @return SimpleEventEmitter
+     */
+    public function unSubscribe(string $eventName, callable $subscriber): SimpleEventEmitter
+    {
+        if (($subscriber instanceof Closure) === false &&
+            ($staticMethod = $this->parseStaticMethod($subscriber)) !== null
+        ) {
+            $subscriber = $this->getUnifiedStaticMethodRepresentation($staticMethod);
+        }
+
+        $eventSubscribers = $this->getEventSubscribers($eventName);
+        $eventSubscribers = array_filter($eventSubscribers, function ($curSubscriber) use ($subscriber) {
+            return $curSubscriber !== $subscriber;
+        });
+
+        return $this->setEventSubscribers($eventName, $eventSubscribers);
+    }
+
+    /**
+     * @return bool
+     */
+    public function isCancellingEnabled(): bool
+    {
+        return $this->cancellingEnabled;
+    }
+
+    /**
+     * @return SimpleEventEmitter
+     */
+    public function enableCancelling(): SimpleEventEmitter
+    {
+        $this->cancellingEnabled = true;
+
+        return $this;
+    }
+
+    /**
+     * @return SimpleEventEmitter
+     */
+    public function disableCancelling(): SimpleEventEmitter
+    {
+        $this->cancellingEnabled = false;
+
+        return $this;
+    }
+
+    /**
+     * @param array $subscribers
+     *
+     * @return SimpleEventEmitter
+     */
+    public function setStaticSubscribers(array $subscribers): SimpleEventEmitter
+    {
+        assert($this->checkAllSubscribersAreStatic($subscribers) === true);
+
+        return $this->setSubscribers($subscribers);
+    }
+
+    /**
+     * @return array
+     */
+    public function getStaticSubscribers(): array
+    {
+        $result = [];
+
+        foreach ($this->getSubscribers() as $eventName => $subscribersList) {
+            $eventSubscribers = [];
+            foreach ($subscribersList as $subscriber) {
+                if (($staticMethod = $this->parseStaticMethod($subscriber)) !== null) {
+                    $eventSubscribers[] = $this->getUnifiedStaticMethodRepresentation($staticMethod);
+                }
+            }
+
+            if (empty($eventSubscribers) === false) {
+                $result[$eventName] = $eventSubscribers;
+            }
+        }
+
+        return $result;
+    }
+
+    /**
+     * @return array
+     */
+    protected function getSubscribers(): array
+    {
+        return $this->subscribers;
+    }
+
+    /**
+     * @param callable[] $subscribers
+     *
+     * @return SimpleEventEmitter
+     */
+    protected function setSubscribers(array $subscribers): SimpleEventEmitter
+    {
+        $this->subscribers = $subscribers;
+
+        return $this;
+    }
+
+    /**
+     * @param string $eventName
+     * @param array  $arguments
+     *
+     * @return void
+     */
+    protected function emitWithoutCancellingPropagationCheck(string $eventName, array $arguments = [])
+    {
+        foreach ($this->getEventSubscribers($eventName) as $subscriber) {
+            call_user_func_array($subscriber, $arguments);
+        }
+    }
+
+    /**
+     * @param string $eventName
+     * @param array  $arguments
+     *
+     * @return void
+     */
+    protected function emitWithCancellingPropagationCheck(string $eventName, array $arguments = [])
+    {
+        foreach ($this->getEventSubscribers($eventName) as $subscriber) {
+            if (call_user_func_array($subscriber, $arguments) === false) {
+                break;
+            }
+        }
+    }
+
+    /**
+     * @param string $eventName
+     *
+     * @return array
+     */
+    private function getEventSubscribers(string $eventName): array
+    {
+        $subscribers = $this->getSubscribers();
+        if (array_key_exists($eventName, $subscribers) === false) {
+            throw new EventNotFoundException($eventName);
+        }
+
+        $result = $subscribers[$eventName];
+
+        return $result;
+    }
+
+    /**
+     * @param string     $eventName
+     * @param callable[] $eventSubscribers
+     *
+     * @return SimpleEventEmitter
+     */
+    private function setEventSubscribers(string $eventName, array $eventSubscribers): SimpleEventEmitter
+    {
+        $this->subscribers[$eventName] = $eventSubscribers;
+
+        return $this;
+    }
+
+    /**
+     * This debugging function checks subscribers are
+     * [
+     *     ...
+     *     'string_event_name' => [static callable, static callable, ...],
+     *     ...
+     * ]
+     *
+     * @param array $subscribers
+     *
+     * @return bool
+     */
+    private function checkAllSubscribersAreStatic(array $subscribers): bool
+    {
+        $result = true;
+        foreach ($subscribers as $eventName => $callableList) {
+            if (is_string($eventName) === false || is_array($callableList) === false) {
+                $result = false;
+                break;
+            }
+            foreach ($callableList as $mightBeCallable) {
+                $method = $this->parseStaticMethod($mightBeCallable);
+                if ($method === null || $method->isStatic() === false) {
+                    $result = false;
+                    break;
+                }
+            }
+        }
+
+        return $result;
+    }
+
+    /**
+     * @param $mightBeCallable
+     *
+     * @return null|ReflectionMethod
+     *
+     * @SuppressWarnings(PHPMD.ElseExpression)
+     */
+    private function parseStaticMethod($mightBeCallable)
+    {
+        // static callable could be in form of 'ClassName::methodName' or ['ClassName', 'methodName']
+        if (is_string($mightBeCallable) === true &&
+            count($mightBeCallablePair = explode('::', $mightBeCallable, 2)) === 2
+        ) {
+            list ($mightBeClassName, $mightBeMethodName) = $mightBeCallablePair;
+        } elseif (is_array($mightBeCallable) === true && count($mightBeCallable) === 2) {
+            list ($mightBeClassName, $mightBeMethodName) = $mightBeCallable;
+        } else {
+            return null;
+        }
+
+        try {
+            $reflectionMethod = new ReflectionMethod($mightBeClassName, $mightBeMethodName);
+        } catch (ReflectionException $exception) {
+            return null;
+        }
+
+        if ($reflectionMethod->isStatic() === false) {
+            return null;
+        }
+
+        return $reflectionMethod;
+    }
+
+    /**
+     * @param ReflectionMethod $staticMethod
+     *
+     * @return callable
+     */
+    private function getUnifiedStaticMethodRepresentation(ReflectionMethod $staticMethod): callable
+    {
+        return [$staticMethod->class, $staticMethod->name];
+    }
+}
---
+++ components/Events/tests/SimpleEventEmitterTest.php
@@ -0,0 +1,199 @@
+<?php namespace Limoncello\Tests\Events;
+
+/**
+ * Copyright 2015-2017 info@neomerx.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+use Limoncello\Events\SimpleEventEmitter;
+use PHPUnit\Framework\TestCase;
+use ReflectionMethod;
+
+/**
+ * @package Limoncello\Tests\Events
+ */
+class SimpleEventEmitterTest extends TestCase
+{
+    /** Method name */
+    const PUBLIC_STATIC_NAME = 'publicStaticHandler';
+
+    /** Method name */
+    const PUBLIC_NON_STATIC_NAME = 'publicNonStaticHandler';
+
+    /**
+     * @var bool
+     */
+    private static $publicStaticCalled = false;
+
+    /**
+     * @var bool
+     */
+    private $publicNonStaticCalled = false;
+
+    /**
+     * Stub for public static handler.
+     *
+     * @return bool
+     */
+    public static function publicStaticHandler(): bool
+    {
+        static::$publicStaticCalled = true;
+
+        return false;
+    }
+
+    /**
+     * Stub for public static handler.
+     *
+     * @return bool
+     */
+    public function publicNonStaticHandler(): bool
+    {
+        $this->publicNonStaticCalled = true;
+
+        return false;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    protected function setUp()
+    {
+        parent::setUp();
+
+        $this->resetCalledFlags();
+    }
+
+    /**
+     * Test basic event operations.
+     *
+     * @return void
+     */
+    public function testBasicSubUnSub()
+    {
+        $emitter = new SimpleEventEmitter();
+        $this->assertTrue($emitter->enableCancelling()->isCancellingEnabled());
+        $this->assertFalse($emitter->disableCancelling()->isCancellingEnabled());
+
+        $emitter->subscribe('event1', [self::class, self::PUBLIC_STATIC_NAME]);
+        $emitter->subscribe('event2', [$this, self::PUBLIC_NON_STATIC_NAME]);
+
+        $this->assertEquals([
+            'event1' => [[self::class, self::PUBLIC_STATIC_NAME], ],
+        ], $emitter->getStaticSubscribers());
+
+        $emitter->emit('event1');
+        $this->assertTrue(static::$publicStaticCalled);
+        $this->assertFalse($this->publicNonStaticCalled);
+
+        $emitter->emit('event2');
+        $this->assertTrue($this->publicNonStaticCalled);
+
+        $this->resetCalledFlags();
+
+        $emitter->unSubscribe('event1', [self::class, self::PUBLIC_STATIC_NAME]);
+        $emitter->unSubscribe('event2', [$this, self::PUBLIC_NON_STATIC_NAME]);
+
+        $emitter->emit('event1');
+        $emitter->emit('event2');
+        $this->assertFalse(static::$publicStaticCalled);
+        $this->assertFalse($this->publicNonStaticCalled);
+    }
+
+    /**
+     * Test cancelling event propagation.
+     */
+    public function testCancellingPropagation()
+    {
+        $eventName = 'event1';
+        $emitter   = (new SimpleEventEmitter())->disableCancelling();
+        $emitter->subscribe($eventName, [self::class, self::PUBLIC_STATIC_NAME]);
+        $emitter->subscribe($eventName, [$this, self::PUBLIC_NON_STATIC_NAME]);
+
+        $emitter->emit($eventName);
+        $this->assertTrue(static::$publicStaticCalled);
+        $this->assertTrue($this->publicNonStaticCalled);
+
+        $this->resetCalledFlags();
+        $emitter->enableCancelling();
+
+        $emitter->emit($eventName);
+        $this->assertTrue(static::$publicStaticCalled);
+        $this->assertFalse($this->publicNonStaticCalled);
+    }
+
+    /**
+     * Test using cache for static handlers.
+     */
+    public function testCacheForStaticHandlers()
+    {
+        $eventName = 'event1';
+        $emitter1  = (new SimpleEventEmitter())->disableCancelling();
+        $emitter1->subscribe($eventName, [self::class, self::PUBLIC_STATIC_NAME]);
+        $emitter1->subscribe($eventName, [$this, self::PUBLIC_NON_STATIC_NAME]);
+        $emitter1->subscribe($eventName, self::class . '::' . self::PUBLIC_STATIC_NAME);
+
+        $cache = $emitter1->getStaticSubscribers();
+
+        $emitter2 = (new SimpleEventEmitter())->disableCancelling()->setStaticSubscribers($cache);
+
+        $emitter2->emit($eventName);
+        $this->assertTrue(static::$publicStaticCalled);
+        $this->assertFalse($this->publicNonStaticCalled);
+    }
+
+    /**
+     * Test emitting non existing event should fail.
+     *
+     * @expectedException \Limoncello\Events\Exceptions\EventNotFoundException
+     */
+    public function testEmitNonExistingEvent()
+    {
+        (new SimpleEventEmitter())->emit('event1');
+    }
+
+    /**
+     * Test invalid subscribers are handled correctly.
+     */
+    public function testCheckInvalidSubscribers1()
+    {
+        $this->assertFalse($this->callCheckSubscribers(['whatever']));
+        $this->assertFalse($this->callCheckSubscribers(['some_even_name' => 'rubbish_insteadof_method_list']));
+        $this->assertFalse($this->callCheckSubscribers(['some_even_name' => ['rubbish_insteadof_method']]));
+        $this->assertFalse($this->callCheckSubscribers(['some_even_name' => [[self::class, 'invalid_method']]]));
+    }
+
+    /**
+     * Reset flags.
+     */
+    private function resetCalledFlags()
+    {
+        static::$publicStaticCalled  = false;
+        $this->publicNonStaticCalled = false;
+    }
+
+    /**
+     * @param array $subscribers
+     *
+     * @return bool
+     */
+    private function callCheckSubscribers(array $subscribers): bool
+    {
+        $method = new ReflectionMethod(SimpleEventEmitter::class, 'checkAllSubscribersAreStatic');
+        $method->setAccessible(true);
+        $result = $method->invoke(new SimpleEventEmitter(), $subscribers);
+
+        return $result;
+    }
+}
