699325e19d0a0bcc2c09b6728aa5637fd23ccd85
EK <ek@neomerx.com>
2017-04-28 21:22:58 +0300

    Add parallel execution.

--- split/split-with-tags.sh
+++ split/split-with-tags.sh
@@ -1,14 +1,28 @@
 #!/usr/bin/env bash
 
 CURRENT_DIR="$(dirname "$0")"
+TMP_DIR="$(mktemp --tmpdir=/dev/shm/ -d)"
+THREAD_ID=0
 
 source "${CURRENT_DIR}/components.sh"
 
-git subsplit init git@github.com:limoncello-php/framework.git
+rm -rf .subsplit/ && git subsplit init git@github.com:limoncello-php/framework.git
 
 for component in "${COMPONENTS[@]}"
 do
-  git subsplit publish --heads="${BRANCHES}" "${component}"
+  ((THREAD_ID++))
+  THREAD_FOLDER=${TMP_DIR}/${THREAD_ID}
+  (\
+    mkdir ${THREAD_FOLDER} &&\
+    cp -r ${CURRENT_DIR}/.subsplit/ ${THREAD_FOLDER} &&\
+    cd ${THREAD_FOLDER} &&\
+    git subsplit publish --heads="${BRANCHES}" "${component}" -q \
+  ) &
 done
 
-rm -rf .subsplit/
+for job in `jobs -p`
+do
+  wait $job
+done
+
+rm -rf .subsplit/ && rm -rf ${TMP_DIR}
