48c7fdd50452dd2c9f84f257732d68fbf84614d6
EK <ek@neomerx.com>
2017-05-11 00:46:35 +0300

    #42 Add test coverage.

---
+++ components/Events/tests/ContainerConfiguratorTest.php
@@ -0,0 +1,67 @@
+<?php namespace Limoncello\Tests\Events;
+
+/**
+ * Copyright 2015-2017 info@neomerx.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+use Limoncello\Contracts\Container\ContainerInterface;
+use Limoncello\Contracts\Settings\SettingsProviderInterface;
+use Limoncello\Events\Contracts\EventEmitterInterface;
+use Limoncello\Events\Package\EventProvider;
+use Limoncello\Events\Package\EventSettings as BaseEventSettings;
+use Limoncello\Tests\Events\Data\EventSettings;
+use Limoncello\Tests\Events\Data\TestContainer;
+use Mockery;
+use Mockery\Mock;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @package Limoncello\Tests\Events
+ */
+class ContainerConfiguratorTest extends TestCase
+{
+    /**
+     * Test provider.
+     */
+    public function testEventProvider()
+    {
+        list($configurator) = EventProvider::getContainerConfigurators();
+        $container    = new TestContainer();
+
+        $this->addSettings($container, BaseEventSettings::class, (new EventSettings())->get());
+
+        $configurator($container);
+
+        $this->assertNotNull($container->get(EventEmitterInterface::class));
+    }
+
+    /**
+     * @param ContainerInterface $container
+     * @param string             $settingsClass
+     * @param array              $settings
+     *
+     * @return self
+     */
+    private function addSettings(ContainerInterface $container, string $settingsClass, array $settings): self
+    {
+        /** @var Mock $settingsMock */
+        $settingsMock = Mockery::mock(SettingsProviderInterface::class);
+        $settingsMock->shouldReceive('get')->once()->with($settingsClass)->andReturn($settings);
+
+        $container->offsetSet(SettingsProviderInterface::class, $settingsMock);
+
+        return $this;
+    }
+}
---
+++ components/Events/tests/Data/TestContainer.php
@@ -0,0 +1,88 @@
+<?php namespace Limoncello\Tests\Events\Data;
+
+/**
+ * Copyright 2015-2017 info@neomerx.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+use Closure;
+use Limoncello\Contracts\Container\ContainerInterface;
+
+/**
+ * @package Limoncello\Tests\Events
+ */
+class TestContainer implements ContainerInterface
+{
+    /**
+     * @var array
+     */
+    private $data = [];
+
+    /**
+     * @inheritdoc
+     */
+    public function get($id)
+    {
+        $valueOrClosure = $this->data[$id];
+
+        if ($valueOrClosure instanceof Closure) {
+            $value = $valueOrClosure($this);
+            $this->offsetSet($id, $value);
+        } else {
+            $value = $valueOrClosure;
+        }
+
+        return $value;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function has($id)
+    {
+        return array_key_exists($id, $this->data);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function offsetExists($offset)
+    {
+        return $this->has($offset);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function offsetGet($offset)
+    {
+        return $this->get($offset);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function offsetSet($offset, $value)
+    {
+        $this->data[$offset] = $value;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function offsetUnset($offset)
+    {
+        unset($this->data[$offset]);
+    }
+}
